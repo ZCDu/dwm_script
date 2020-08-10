b8a7fc2a41513e75aa775f0f932337a5f5625ad8
zcdu <437677825@qq.com>
2020-08-10 18:52:59 +0800

    my script to dwm

---
+++ autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+xrandr --auto --output HDMI1 --size 1920x1080  #设置分辨率，接在上面的后面
+/bin/zsh ~/scripts/dwm-status.sh &
+/bin/zsh ~/scripts/wp-change.sh &
+#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+picom --vsync="false" -b
+#/bin/bash ~/scripts/tap-to-click.sh &
+#/bin/bash ~/scripts/inverse-scroll.sh &
+/bin/zsh ~/scripts/setxmodmap-qwerty.sh &
+nm-applet &
+#xfce4-power-manager &
+dde-volumed-pulse &
+#/bin/bash ~/scripts/run-mailsync.sh &
+/bin/zsh ~/scripts/autostart_wait.sh &
---
+++ sck-tog.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+result=$(ps ax|grep -v grep|grep screenkey)
+if [ "$result" == "" ]; then
+  eval "screenkey --bg-color white --font-color black &"
+else
+  eval "killall screenkey"
+fi
