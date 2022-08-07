326afbc40dbe5695a78da998a795bd86ee34b76c
3090 <my_server@edu.cn>
2022-08-07 17:27:20 +0800

    feat(volume): fix the function of volume, showing current value

--- dwm-status-refresh.sh
+++ dwm-status-refresh.sh
@@ -38,7 +38,7 @@ old_transmitted_bytes=$transmitted_bytes
 old_time=$now
 
 print_volume() {
-	volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
+	volume="$(amixer get IEC958 | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
 	if test "$volume" -gt 0
 	then
 		echo -e "\uE05D${volume}"
