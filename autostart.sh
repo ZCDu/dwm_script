1eabae3d37029f3abcf3acd90d1ab567ff593b84
3090 <my_server@edu.cn>
2023-03-25 15:53:01 +0800

    feat(desplay): change to use one monitor

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-xrandr --auto --output DP-4 --right-of DP-2 --mode 1080x1920 #设置分辨率，接在上面的后面
+# xrandr --auto --output DP-4 --right-of DP-2 --mode 1080x1920 #设置分辨率，接在上面的后面
+xrandr --auto --output DP-4  #设置分辨率，接在上面的后面
 /bin/zsh ~/software/dwmscripts/dwm-status.sh &
 /bin/zsh ~/software/dwmscripts/wp-change.sh &
 # picom -o 0.95 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
